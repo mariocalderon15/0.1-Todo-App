f9377dc96359d5049a3ee411547c546f430435a5
Mario Calderon <mariocalderonalberto@gmail.com>
2026-03-19 12:35:27 +0100

    Reorganizar proyecto y limpiar carpeta interna

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-
-
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: '/0.1-Todo-App/',
+  base: '/0.1-Todo-App/', // la subcarpeta donde se sirve la página en GitHub Pages
+  build: {
+    rollupOptions: {
+      input: {
+        login: 'index.html',
+        todo: 'todo.html',
+      }
+    }
+  }
 });
