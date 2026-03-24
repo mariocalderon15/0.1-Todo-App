333bbf28f8b0772f3cba3595bf753f98e9a0f8e9
Mario Calderon <mariocalderonalberto@gmail.com>
2026-03-24 17:52:07 +0100

    nuevos diseños con css y toggle de contraseña

--- src/main-login.js
+++ src/main-login.js
@@ -1,26 +1,29 @@
 const loginFrom = document.getElementById('login-form');
-
-loginFrom.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    const username = document.getElementById('username').value.toLowerCase();
-    const password = document.getElementById('password').value;
-
-    if(username === 'mario calderon' && password === '1234'){
-        localStorage.setItem('isLoggedIn', 'true');
-        window.location.href = 'todo.html';
-    }else {
-        alert('Usuario o Contraseña incorrectos');
-    }
-});
-
+const loginError = document.getElementById('loginError');
 const passwordInput = document.querySelector('#password');
 const toggleBtn = document.querySelector('#togglePassword');
 const icon = toggleBtn.querySelector('i');
 
-toggleBtn.addEventListener('click', ()=>{
-const isPassword = passwordInput.type === 'password';
-passwordInput.type = isPassword ? 'text' : 'password';
+loginFrom.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const username = document.getElementById('username').value.toLowerCase();
+    const password = document.getElementById('password').value;
 
-icon.classList.toggle('bi-eye');
-icon.classList.toggle('bi-eye-slash');
+    if (username === 'mario calderon' && password === '1234') {
+        localStorage.setItem('isLoggedIn', 'true');
+            window.location.href = 'todo.html';
+       
+    } else {
+        loginError.textContent = 'Usuario o Contraseña incorrectos';
+        loginError.style.display = 'block';
+        passwordInput.value = '';
+    }
+});
+
+toggleBtn.addEventListener('click', () => {
+    const isPassword = passwordInput.type === 'password';
+    passwordInput.type = isPassword ? 'text' : 'password';
+
+    icon.classList.toggle('bi-eye');
+    icon.classList.toggle('bi-eye-slash');
 });
